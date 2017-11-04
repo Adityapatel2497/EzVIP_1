db67246b3e28131fb43494cd9d0e3ff0e9f5b87d
Yash Karanke <dex.papa@gmail.com>
2017-11-04 10:16:55 +0530

    new update

--- DBConnection.php
+++ DBConnection.php
@@ -1,7 +1,7 @@
 <?php
 //Establishing connection with the database
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'rooot');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'ezvip'); //where ezvip is the database
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
